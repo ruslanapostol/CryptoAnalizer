584265c0b55ff8b9ee6b9a2544421a889c41d54e
ruslanapostol <apostolruslan.java@gmail.com>
2024-04-30 15:30:20 +0300

    removed redundant constructors

--- src/main/java/org/javarush/apostol/cryptoanalizer/exception/AppException.java
+++ src/main/java/org/javarush/apostol/cryptoanalizer/exception/AppException.java
@@ -2,21 +2,8 @@ package org.javarush.apostol.cryptoanalizer.exception;
 
 
 public class AppException extends RuntimeException {
-
-    public AppException() {
-        super();
-    }
-
-    public AppException(String message) {
-        super(message);
-    }
-
     public AppException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public AppException(Throwable cause) {
-        super(cause);
     }
 }
 
